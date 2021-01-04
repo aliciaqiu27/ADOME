0cf8dc9cc12cf4c1a1c276ac5a9f3768bcca1337
Alicia Qiu Cheung <aliciaqiu27@gmail.com>
2021-01-03 19:26:31 -0700

    added on-click function to allow pets to change.

---
+++ html_old.js
@@ -0,0 +1,201 @@
+// // google maps api 
+// let map, infoWindow;
+// var apiKey = ""
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 6,
+//   });
+//   infoWindow = new google.maps.InfoWindow();
+//   const locationButton = document.createElement("button");
+//   locationButton.textContent = "Pan to Current Location";
+//   locationButton.classList.add("custom-map-control-button");
+//   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+//   locationButton.addEventListener("click", () => {
+//     // Try HTML5 geolocation.
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//           const pos = {
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude,
+//           };
+//           infoWindow.setPosition(pos);
+//           infoWindow.setContent("Location found.");
+//           infoWindow.open(map);
+//           map.setCenter(pos);
+//         },
+//         () => {
+//           handleLocationError(true, infoWindow, map.getCenter());
+//         }
+//       );
+//     } else {
+//       // Browser doesn't support Geolocation
+//       handleLocationError(false, infoWindow, map.getCenter());
+//     }
+//   });
+// }
+
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(
+//     browserHasGeolocation
+//       ? "Error: The Geolocation service failed."
+//       : "Error: Your browser doesn't support geolocation."
+//   );
+//   infoWindow.open(map);
+// }
+
+
+// //zipcode application
+//     var clientKey = "js-9qZHzu2Flc59Eq5rx10JdKERovBlJp3TQ3ApyC4TOa3tA8U7aVRnFwf41RpLgtE7";
+		
+// 		var cache = {};
+// 		var container = $("#example1");
+// 		var errorDiv = container.find("div.text-error");
+		
+// 		/** Handle successful response */
+// 		function handleResp(data)
+// 		{
+// 			// Check for error
+// 			if (data.error_msg)
+// 				errorDiv.text(data.error_msg);
+// 			else if ("city" in data)
+// 			{
+// 				// Set city and state
+// 				container.find("input[name='city']").val(data.city);
+// 				container.find("input[name='state']").val(data.state);
+// 			}
+// 		}
+		
+// 		// Set up event handlers
+// 		container.find("input[name='zipcode']").on("keyup change", function() {
+// 			// Get zip code
+// 			var zipcode = $(this).val().substring(0, 5);
+// 			if (zipcode.length == 5 && /^[0-9]+$/.test(zipcode))
+// 			{
+// 				// Clear error
+// 				errorDiv.empty();
+				
+// 				// Check cache
+// 				if (zipcode in cache)
+// 				{
+// 					handleResp(cache[zipcode]);
+// 				}
+// 				else
+// 				{
+// 					// Build url
+// 					var url = "https://www.zipcodeapi.com/rest/"+clientKey+"/info.json/" + zipcode + "/radians";
+					
+// 					// Make AJAX request
+// 					$.ajax({
+// 						"url": url,
+// 						"dataType": "json"
+// 					}).done(function(data) {
+// 						handleResp(data);
+						
+// 						// Store in cache
+// 						cache[zipcode] = data;
+// 					}).fail(function(data) {
+// 						if (data.responseText && (json = $.parseJSON(data.responseText)))
+// 						{
+// 							// Store in cache
+// 							cache[zipcode] = json;
+							
+// 							// Check for error
+// 							if (json.error_msg)
+// 								errorDiv.text(json.error_msg);
+// 						}
+// 						else
+// 							errorDiv.text('Request failed.');
+// 					});
+// 				}
+// 			}
+// 		}).trigger("change");
+
+
+
+
+//pet api
+// var settings = {
+//   "url": "https://api.rescuegroups.org/v5/public/animals/search/available/",
+//   "method": "POST",
+//   "timeout": 0,
+//   "headers": {
+//     "Content-Type": "application/vnd.api+json",
+//     "Authorization": "TGBdZnRM"
+//   },
+//   // "data": "{\n    \"data\": {\n        \"filters\": \n    \t[\n    \t\t{\n    \t\t\t\"fieldName\": \"animals.id\",\n    \t\t\t\"operation\": \"equal\",\n    \t\t\t\"criteria\": \"10006340\"\n    \t\t},\n    \t\t{\n    \t\t\t\"fieldName\": \"animals.sizeGroup\",\n    \t\t\t\"operation\": \"equal\",\n    \t\t\t\"criteria\": [\"Small\",\"Medium\"]\n    \t\t}\n    \t],\n    \t\"filterProcessing\": \"1 and 2\",\n        \"filterRadius\":\n        \t{\n        \t\t\"miles\": 100,\n        \t\t\"lat\": 34.1031,\n        \t\t\"lon\": -118.416\n        \t}\n        \n    }\n}\n",
+// };
+
+
+$("#left").on("click", function(event) {
+
+        // Here, it prevents the submit button from trying to submit a form when clicked
+        event.preventDefault();
+        $("#petImages").empty();
+
+
+// $.ajax(settings).done(function (response) {
+
+
+$.ajax({
+    url:
+        "https://api.rescuegroups.org/v5/public/animals/search/available/",
+    headers: { Authorization: "TGBdZnRM"  },
+}).then(function (response) {
+console.log(response);
+// console.log(response.data[0].attributes.adoptionFeeString);
+
+
+var descriptionText = response.data[0].attributes.descriptionText;
+
+console.log(descriptionText);
+
+$(".descriptionTextDiv").append(descriptionText)
+
+// for (let i = 0; i < response.data.length; i++) {
+//   var picturesId = JSON.stringify(response.data[i].relationships.pictures.data.id);
+//   console.log(picturesId);
+  
+// }
+// for (let i = 0; i < picturesId.length; i++) {
+//   const element = picturesId.data[i].id;
+// }
+
+var myArray = [];
+for(var i = 0, len = response.data.length; i < len; i++){
+  myArray.push(response.data[i].relationships.pictures[2]);
+}
+
+console.log(myArray);
+
+for (let i = 0; i < response.included.length; i++) {
+  const picture = response.included[i].type;
+  // console.log(picture);
+  const createdDate = response.included[i].attributes.created;
+  // console.log(createdDate);
+
+  // var data = response.data[i].relationships.pictures.data[i].id;
+
+  // console.log(picture);
+  // console.log(data);
+
+
+  
+  if (picture==="pictures") {
+    const pictureArray = response.included[i].attributes.large.url;
+    console.log(pictureArray);
+    const pictureFinal = $("<img>").attr("src", pictureArray);
+    const pictureResized = pictureFinal.addClass("pictureFinalArray");
+    // console.log(pictureArray);
+    $("#petImages").append(pictureResized)
+  }
+}
+
+
+
+
+});
+});
